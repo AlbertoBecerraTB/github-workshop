5244026f62e42a1edaa8ab550aeae120cd5d10e2
AlbertoBecerraTB <101355541+AlbertoBecerraTB@users.noreply.github.com>
2023-03-01 17:34:47 +0100

    cambio conflicto

--- src/julio_suma_cuadrados.py
+++ src/julio_suma_cuadrados.py
@@ -1,6 +1,6 @@
 def suma_cuadrados(a,b):
     '''
-    Esta  suma los cuadrados de dos números a, b y 
+    Esta divertida suma los cuadrados de dos números a, b y 
     devuelve el resultado
     '''
     suma = a**2 + b**2
