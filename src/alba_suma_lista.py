61e02a184b15ce0d1187ff4b3be250a82d12ddb7
Alba <albacruzgal22@gmail.com>
2023-03-03 10:41:52 +0100

    funcion_alba

---
+++ src/alba_suma_lista.py
@@ -0,0 +1,10 @@
+def suma_lista(lista):
+    '''
+    Función que devuelve la suma de todos los elementos de una lista
+    '''
+    suma = 0
+    for num in lista:
+        suma = suma + num 
+    return suma
+
+print(suma_lista([2, 4, 9, 3, 9]))
