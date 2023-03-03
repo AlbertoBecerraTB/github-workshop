9c9beb0ff33e561323659ea8672e4848f3fa70fe
Antonio <antoja97@gmail.com>
2023-03-03 11:02:01 +0100

    Actualizado

--- src/antonio_pares.py
+++ src/antonio_pares.py
@@ -1,10 +1,13 @@
 def antonio_pares(ls):
-
+    '''
+    Función para saber números pares
+    
+    '''
     
     ls1=[]
     for i in ls:
         if i %2==0:
             ls1.append(i)
     return ls1
-print(antonio_pares([1,2,3,4,5]))
+antonio_pares([1,2,3,4,5])
         
