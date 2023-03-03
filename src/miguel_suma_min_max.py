2f30cdad622cd8b9b6b93cef0a35de666337c9eb
Miguel <mgiljimenez@gmail.com>
2023-03-03 10:42:13 +0100

    nueva_funcion_miguel

---
+++ src/miguel_suma_min_max.py
@@ -0,0 +1,11 @@
+def suma_valor_min_max():
+
+    '''
+    Esta función devuelve el sumatorio del valor mínimo
+    y máximo de una lista
+    '''
+    ls1=[4,7,5,3,9]
+    suma=min(ls1)+max(ls1)
+    return suma
+
+print(suma_valor_min_max())
