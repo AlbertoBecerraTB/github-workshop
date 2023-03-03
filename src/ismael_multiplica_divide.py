b79743d7243d43b60124d3f2f9a9c818b4222f6e
IsmaelMerino <ismmerlim@hotmail.com>
2023-03-03 10:37:54 +0100

    ismael

---
+++ src/ismael_multiplica_divide.py
@@ -0,0 +1,8 @@
+def multiplica_divide (a,b):
+    '''
+    Esta  multiplica y divide los 
+    numeros a y b.
+    '''
+    multiplica = a * 2 + b * 2
+    divide = a / multiplica
+    return  divide
